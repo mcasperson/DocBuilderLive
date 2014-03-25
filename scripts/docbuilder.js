3b1349594cdbc99bb1ca5e1dedcc5c5830e53ee1
Matthew Casperson <mcaspers@redhat.com>
2014-03-25 13:23:33 +1000

    Working on DocBuilder Live prototype

--- scripts/docbuilder.js
+++ scripts/docbuilder.js
@@ -1,5 +1,6 @@
 /// <reference path="../definitions/jquery.d.ts" />
 /// <reference path="collections.ts" />
+var RETRY_COUNT = 5;
 var SERVER = "http://pressgang.lab.eng.pnq.redhat.com:8080";
 var REVISION_DETAILS = "/pressgang-ccms/rest/1/sysinfo/get/json";
 
@@ -11,10 +12,32 @@ var RenderedTopicDetails = (function () {
 
 var DocBuilderLive = (function () {
     function DocBuilderLive(specId) {
+        var _this = this;
         this.specId = specId;
+        this.getLastModifiedTime(function (lastRevisionDate) {
+            _this.lastRevisionDate = lastRevisionDate;
+        }, function (title, message) {
+            window.alert(title + "\n" + message);
+        });
     }
-    DocBuilderLive.prototype.getLastModifiedTime = function (callback) {
-        jQuery.ajax();
+    DocBuilderLive.prototype.getLastModifiedTime = function (callback, errorCallback, retryCount) {
+        var _this = this;
+        if (typeof retryCount === "undefined") { retryCount = 0; }
+        jQuery.ajax({
+            type: 'GET',
+            url: SERVER + REVISION_DETAILS,
+            dataType: "json",
+            success: function (data) {
+                callback(new Date(data.lastRevisionDate));
+            },
+            error: function () {
+                if (retryCount < RETRY_COUNT) {
+                    _this.getLastModifiedTime(callback, errorCallback, retryCount);
+                } else {
+                    errorCallback("Connection Error", "An error occurred while getting the server settings. This may be caused by an intermittent network failure. Try your import again, and if problem persist log a bug.");
+                }
+            }
+        });
     };
 
     DocBuilderLive.prototype.getSpec = function (callback) {
--- scripts/docbuilder.ts
+++ scripts/docbuilder.ts
@@ -1,8 +1,14 @@
 /// <reference path="../definitions/jquery.d.ts" />
 /// <reference path="collections.ts" />
 
+var RETRY_COUNT:number = 5;
 var SERVER:string = "http://pressgang.lab.eng.pnq.redhat.com:8080";
 var REVISION_DETAILS:string = "/pressgang-ccms/rest/1/sysinfo/get/json";
+
+interface SysInfo {
+    lastRevision:number;
+    lastRevisionDate:number;
+}
 
 class RenderedTopicDetails {
     public topicId:number;
@@ -19,10 +25,33 @@ class DocBuilderLive {
 
     constructor(specId:number) {
         this.specId = specId;
+        this.getLastModifiedTime(
+            (lastRevisionDate:Date)=>{
+                this.lastRevisionDate = lastRevisionDate;
+            },
+            (title:string, message:string) => {
+                window.alert(title + "\n" + message);
+            }
+        );
     }
 
-    getLastModifiedTime(callback: (lastRevisionDate:Date) => void):void {
-        jQuery.ajax()
+    getLastModifiedTime(callback: (lastRevisionDate:Date) => void, errorCallback: (title:string, message:string) => void, retryCount:number=0):void {
+
+        jQuery.ajax({
+            type: 'GET',
+            url: SERVER + REVISION_DETAILS,
+            dataType: "json",
+            success: (data:SysInfo)=>{
+               callback(new Date(data.lastRevisionDate));
+            },
+            error: ()=>{
+                if (retryCount < RETRY_COUNT) {
+                    this.getLastModifiedTime(callback, errorCallback, retryCount);
+                } else {
+                    errorCallback("Connection Error", "An error occurred while getting the server settings. This may be caused by an intermittent network failure. Try your import again, and if problem persist log a bug.");
+                }
+            }
+        });
     }
 
     getSpec(callback: (topic:Object) => void):void {
