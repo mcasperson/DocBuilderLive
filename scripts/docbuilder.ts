0d395fd78ab76f0560b934d8d9015a6f83d30c72
Matthew Casperson <matthewcasperson@gmail.com>
2014-03-30 08:06:37 +1000

    Working on DocBuilder Live prototype

--- scripts/docbuilder.ts
+++ scripts/docbuilder.ts
@@ -13,7 +13,7 @@ var SPEC_TITLE = "data-title";
 var SPEC_TITLE_CONTAINER = "data-container";
 var LOADING_TOPIC_DIV_CLASS = "loadingTopicDiv";
 /**
- * Used to identify any div that holds spec info
+ * Used to identify any div that holds spec info (topic contents or container title)
  * @type {string}
  */
 var SPEC_DIV_CLASS = "contentSpecDiv";
@@ -28,6 +28,11 @@ var SPEC_TOPIC_DIV_CLASS = "contentSpecTopicDiv";
  */
 var SPEC_TITLE_DIV_CLASS = "contentSpecTitleDiv";
 var IFRAME_ID_PREFIX = "iframeId";
+/**
+ * The ID assigned to a div that holds some spec content. This is followed by an integer (ie divId0)
+ * that defines the position of the div in the linear flow of the book.
+ * @type {string}
+ */
 var DIV_ID_PREFIX = "divId";
 var DIV_BOOK_INDEX_ID_PREFIX = "divBookIndex";
 var LOADING_HTML = "<div style='width: 100%; text-align: center;'>LOADING</div>";
@@ -75,7 +80,7 @@ var SPECS_REST_EXPAND:Object={
     branches: [
         {
             trunk: {
-                name: "items"
+                name: "contentSpecs"
             },
             branches: [
                 {
@@ -107,6 +112,10 @@ function nodeIsContainer(specNode:SpecNode):boolean {
     return CONTAINER_NODE_TYPES.indexOf(specNode.nodeType) !== -1;
 }
 
+function nodeIsTopicOrContainer(specNode:SpecNode):boolean {
+    return nodeIsContainer(specNode) || nodeIsTopic(specNode);
+}
+
 class TreeNode {
     text:string;
     icon:string;
@@ -115,7 +124,7 @@ class TreeNode {
 }
 
 interface SpecNodeCollectionParentItems {
-    items:SpecNodeCollectionParent[];
+    items:SpecNodeCollectionParentItem[];
 }
 
 interface SysInfo {
@@ -129,6 +138,10 @@ interface SpecNodeCollectionParent {
 
 interface SpecNodeCollection {
     items:SpecNodeItem[];
+}
+
+interface SpecNodeCollectionParentItem {
+    item:SpecNodeCollectionParent;
 }
 
 interface SpecNodeItem {
@@ -290,6 +303,30 @@ class DocBuilderLive {
         });
     }
 
+    expandSpec(spec:SpecNodeCollectionParent, callback: (spec:SpecNodeCollectionParent) => void, errorCallback: (title:string, message:string) => void):void {
+        var expandChildren = (index:number):void => {
+            if (index >= spec.children_OTM.items.length) {
+                callback(spec);
+            } else {
+                var element:SpecNode = spec.children_OTM.items[index].item;
+                if (nodeIsContainer(element)) {
+                    this.populateChild(
+                        element.id,
+                        (node:SpecNode):void => {
+                            spec.children_OTM.items[index].item.children_OTM = node.children_OTM;
+                            expandChildren(++index);
+                        },
+                        errorCallback
+                    );
+                } else {
+                    expandChildren(++index);
+                }
+            }
+        }
+
+        expandChildren(0);
+    }
+
     /**
      * Get a spec with all child details expanded
      * @param callback Called with the expanded spec object
@@ -301,28 +338,8 @@ class DocBuilderLive {
             type: 'GET',
             url: SERVER + SPEC_REST + this.specId + "?expand=" + encodeURIComponent(JSON.stringify(SPEC_REST_EXPAND)),
             dataType: "json",
-            success: (data:SpecNodeCollectionParent) => {
-                var expandChildren = (index:number):void => {
-                    if (index >= data.children_OTM.items.length) {
-                        callback(data);
-                    } else {
-                        var element:SpecNode = data.children_OTM.items[index].item;
-                        if (nodeIsContainer(element)) {
-                            this.populateChild(
-                                element.id,
-                                (node:SpecNode):void => {
-                                    data.children_OTM.items[index].item.children_OTM = node.children_OTM;
-                                    expandChildren(++index);
-                                },
-                                errorCallback
-                            );
-                        } else {
-                            expandChildren(++index);
-                        }
-                    }
-                }
-
-                expandChildren(0);
+            success: (data:SpecNodeCollectionParent):void => {
+                this.expandSpec(data, callback, errorCallback);
             },
             error: ()=>{
                 if (retryCount < RETRY_COUNT) {
@@ -355,7 +372,7 @@ class DocBuilderLive {
             });
 
             if (lastChild === undefined && lastInitialTextChild == undefined) {
-                error("Could not find the last child in the linked list");
+                throw "Could not find the last child in the linked list";
                 return;
             }
 
@@ -421,8 +438,7 @@ class DocBuilderLive {
         });
 
         if (lastChild === undefined && lastInitialTextChild == undefined) {
-            error("Could not find the last child in the linked list");
-            return;
+            throw "Could not find the last child in the linked list";
         }
 
         var reverseChildren:SpecNode[] = [];
@@ -562,9 +578,7 @@ class DocBuilderLive {
 
         jQuery("#loading").remove();
 
-        var topicsAndContainers = _.filter(specTopics, function(element):boolean {
-            return nodeIsTopic(element) || nodeIsContainer(element);
-        });
+        var topicsAndContainers = _.filter(specTopics, nodeIsTopicOrContainer);
 
         _.reduce(topicsAndContainers, function(delay:number, element, index) {
             var iFrame = this.buildIFrameAndDiv(element);
@@ -605,7 +619,9 @@ class DocBuilderLive {
             addChildren(element, toc);
         })
 
-        jQuery("#toc").jstree({
+        jQuery("#toc").remove();
+
+        var tocDiv = jQuery("<div id='toc'></div>").jstree({
             'core' : {
                 'multiple' : false,
                 'data' : toc.children
@@ -620,6 +636,7 @@ class DocBuilderLive {
                 }
             }
         });
+        jQuery(document.body).append(tocDiv);
     }
 
     startRefreshCycle():void {
@@ -634,13 +651,21 @@ class DocBuilderLive {
         }
 
         this.getLastModifiedTime(
-            function (lastRevisionDate:Date):void {
+            (lastRevisionDate:Date):void => {
                 this.findUpdatedSpec(
                     (spec:SpecNodeCollectionParentItems):void => {
                         this.lastRevisionDate = lastRevisionDate;
                         if (spec.items.length !== 0) {
-                            var updatedSpec:SpecNodeCollectionParent = spec.items[0];
-                            this.syncDomWithSpec(updatedSpec);
+                            var updatedSpec:SpecNodeCollectionParent = spec.items[0].item;
+                            this.expandSpec(
+                                updatedSpec,
+                                (expandedSpec:SpecNodeCollectionParent):void => {
+                                    this.syncDomWithSpec(expandedSpec);
+                                    this.buildToc(expandedSpec);
+                                },
+                                errorCallback
+                            )
+
                         } else {
                             this.startRefreshCycle();
                         }
@@ -654,7 +679,8 @@ class DocBuilderLive {
     }
 
     findUpdatedSpec(callback: (spec:SpecNodeCollectionParentItems) => void, errorCallback: (title:string, message:string) => void, retryCount:number=0):void {
-        var url = SERVER + SPECS_REST.replace(CONTENT_SPEC_ID_MARKER, this.specId.toString()).replace(CONTENT_SPEC_EDIT_DATE_MARKER, moment.utc(this.lastRevisionDate).format()) +
+        var startEditDate = moment(this.lastRevisionDate).format("YYYY-MM-DDTHH:mm:ss.SSSZ");
+        var url = SERVER + SPECS_REST.replace(CONTENT_SPEC_ID_MARKER, this.specId.toString()).replace(CONTENT_SPEC_EDIT_DATE_MARKER, encodeURIComponent(encodeURIComponent(startEditDate))) +
             "?expand=" + encodeURIComponent(JSON.stringify(SPECS_REST_EXPAND));
 
         jQuery.ajax({
@@ -690,20 +716,24 @@ class DocBuilderLive {
      */
     syncDomWithSpec(updatedSpec:SpecNodeCollectionParent):void {
 
-        function specTopicDivExists(specNode:SpecNode):boolean {
+        function getTopicDiv(specNode:SpecNode):JQuery {
             if (specNode.entityRevision === null) {
-                return jQuery("div[" + SPEC_TOPIC_DIV_ENTITY_ID + "='" + specNode.entityId + "']").length !== 0
+                return jQuery("div[" + SPEC_TOPIC_DIV_ENTITY_ID + "='" + specNode.id + "']");
             } else {
-                return jQuery("div[" + SPEC_TOPIC_DIV_ENTITY_ID + "='" + specNode.entityId + "'][" + SPEC_TOPIC_DIV_ENTITY_REV + "='" + specNode.entityRevision + "']").length !== 0
+                return jQuery("div[" + SPEC_TOPIC_DIV_ENTITY_ID + "='" + specNode.id + "'][" + SPEC_TOPIC_DIV_ENTITY_REV + "='" + specNode.revision + "']");
             }
         }
 
+        function getTitleDiv(specNode:SpecNode):JQuery {
+            return jQuery("div[" + SPEC_TITLE + "='" + specNode.title + "'][" + SPEC_TITLE_CONTAINER + "='" + specNode.nodeType.toLowerCase() + "']");
+        }
+
+        function specTopicDivExists(specNode:SpecNode):boolean {
+            return getTopicDiv(specNode).length !== 0;
+        }
+
         function specTitleDivExists(specNode:SpecNode):boolean {
-            if (specNode.entityRevision === null) {
-                return jQuery("div[" + SPEC_TITLE + "='" + specNode.title + "']").length !== 0
-            } else {
-                return jQuery("div[" + SPEC_TITLE_CONTAINER + "='" + specNode.nodeType + "'][" + SPEC_TOPIC_DIV_ENTITY_REV + "='" + specNode.entityRevision + "']").length !== 0
-            }
+            return getTitleDiv(specNode).length !== 0;
         }
 
         var specNodes = this.getAllChildrenInFlatOrder(updatedSpec);
@@ -712,17 +742,15 @@ class DocBuilderLive {
             Remove any existing topics that are no longer present.
          */
         var existingSpecDivs = jQuery("." + SPEC_DIV_CLASS);
-        var removeDivList:JQuery[] = [];
-        _.each(existingSpecDivs, function(element) {
+        var removeDivList = _.filter(existingSpecDivs, function(element) {
             var jQueryElement = jQuery(element);
-
             if (jQueryElement.hasClass(SPEC_TOPIC_DIV_CLASS)) {
                 /*
-                    This div displays some topic info
-                */
+                 This div displays some topic info
+                 */
 
-                var entityId = parseInt(jQueryElement.attr(SPEC_TOPIC_DIV_ENTITY_ID));
-                var entityRev = jQueryElement.attr(SPEC_TOPIC_DIV_ENTITY_REV) !== null ?
+                var nodeId = parseInt(jQueryElement.attr(SPEC_TOPIC_DIV_ENTITY_ID));
+                var nodeRev = jQueryElement.attr(SPEC_TOPIC_DIV_ENTITY_REV) !== null ?
                     parseInt(jQueryElement.attr(SPEC_TOPIC_DIV_ENTITY_REV)) :
                     null;
 
@@ -731,24 +759,22 @@ class DocBuilderLive {
                         return false;
                     }
 
-                    if (specNode.entityId !== entityId) {
+                    if (specNode.id !== nodeId) {
                         return false;
                     }
 
-                    if (specNode.entityRevision !== entityRev) {
+                    if (specNode.revision !== nodeRev) {
                         return false;
                     }
 
                     return true;
                 });
 
-                if (!existingNode) {
-                    removeDivList.push(jQueryElement);
-                }
+                return existingNode === null;
             } else if (jQueryElement.hasClass(SPEC_TITLE_DIV_CLASS)) {
                 /*
-                    This div displays some title info
-                */
+                 This div displays some title info
+                 */
 
                 var title = jQueryElement.attr(SPEC_TITLE);
                 var container = jQueryElement.attr(SPEC_TITLE_CONTAINER);
@@ -765,35 +791,108 @@ class DocBuilderLive {
                     return true;
                 });
 
-                if (!existingNode) {
-                    removeDivList.push(jQueryElement);
-                }
+                return existingNode === null;
             }
+
+            return false;
         });
 
         _.each(removeDivList, function(element) {
             /*
                 Remove any link target div associated with the spec div
              */
-            var linkTargets:JQuery[] = element[0][SPEC_DIV_LINK_TARGETS_PROPERTY];
+            var linkTargets:JQuery[] = element[SPEC_DIV_LINK_TARGETS_PROPERTY];
             _.each(linkTargets, function(linkTarget) {
                 linkTarget.remove();
             });
 
-            element.remove();
+            jQuery(element).remove();
         });
 
         /*
             Create new iframes and divs for missing topics and titles
          */
-        var specNodeMissingDiv = _.filter(specNodes, function(specNode) {
+        var specNodesMissingDiv = _.filter(specNodes, function(specNode) {
             if (nodeIsTopic(specNode)) {
                 return !specTopicDivExists(specNode);
             } else if (nodeIsContainer(specNode)) {
                 return !specTitleDivExists(specNode);
             }
         });
+
+        _.reduce(specNodesMissingDiv, function(delay:number, element, index) {
+            var iFrame = this.buildIFrameAndDiv(element);
+            this.setIFrameSrc(iFrame, delay, index);
+            return delay + DELAY_BETWEEN_IFRAME_SRC_CALLS;
+        }, 0, this);
+
+        function divsAreEqual(node1:JQuery, node2:JQuery):boolean {
+            if (node1.length !== 1) {
+                return false;
+            }
+
+            if (node2.length !== 1) {
+                return false;
+            }
+
+            return node1[0].id === node2[0].id;
+        }
+
+        function getActualChild(specNode):JQuery {
+            if (nodeIsTopic(specNode)) {
+                return getTopicDiv(specNode);
+
+            } else if (nodeIsContainer(specNode)) {
+                return getTitleDiv(specNode);
+            }
+
+            return null;
+        }
+
+        /*
+            Reorganise DOM to match new spec
+         */
+        var topicsAndContainers = _.filter(specNodes, nodeIsTopicOrContainer);
+        _.each(topicsAndContainers, function(specNode, index, list) {
+            var nthSpecDiv = jQuery("div." + SPEC_DIV_CLASS + ":eq(" + index + ")");
+            var previousSibling = index === 0 ? null : jQuery("div." + SPEC_DIV_CLASS + ":eq(" + (index - 1) + ")");
+            var actualChild = getActualChild(specNode);
+
+            if (actualChild == null || actualChild.length !== 1) {
+                throw "actualChild.length should always be 1, because all divs should be attached to the dom at this point";
+            }
+
+            if (nthSpecDiv.length !== 1) {
+                throw "nthSpecDiv.length should always be 1, because all divs should be attached to the dom at this point";
+            }
+
+            if (!divsAreEqual(nthSpecDiv, actualChild)) {
+                actualChild.remove();
+                _.each(actualChild[SPEC_DIV_LINK_TARGETS_PROPERTY], function(linkTarget:JQuery) {
+                    linkTarget.remove();
+                });
+
+                if (previousSibling === null) {
+                    jQuery(document.body).prepend(actualChild);
+                    _.each(actualChild[SPEC_DIV_LINK_TARGETS_PROPERTY], function(linkTarget:JQuery) {
+                        jQuery(document.body).prepend(linkTarget);
+                    });
+                } else {
+                    previousSibling.after(actualChild);
+                    _.each(actualChild[SPEC_DIV_LINK_TARGETS_PROPERTY], function(linkTarget:JQuery) {
+                        previousSibling.after(linkTarget);
+                    });
+                }
+            }
+        });
+
+        /*
+            Update the ids to reflect the position of the divs in the book.
+         */
+        _.each(jQuery("div." + SPEC_DIV_CLASS), function(element, index, list) {
+            element.id = DIV_ID_PREFIX + index;
+        });
     }
 }
 
-new DocBuilderLive(21464);
+var docBuilderLive = new DocBuilderLive(21464);
