d74e26146e6ffa9a8324631efbce562767a4df60
Matthew Casperson <matthewcasperson@gmail.com>
2014-05-26 16:16:39 +1000

    Started work on AngularJS integration

--- src/main/webapp/scripts/speclistcontroller.ts
+++ src/main/webapp/scripts/speclistcontroller.ts
@@ -1,10 +1,35 @@
 /// <reference path="../definitions/angular.d.ts" />
 /// <reference path="constants.ts" />
 
+var SPEC_COLLECTION_EXPAND:Object={
+    branches: [
+        {
+            trunk: {
+                name: "contentSpecs"
+            },
+            branches: [
+                {
+                    trunk: {
+                        name: "children_OTM"
+                    }
+                }
+            ]
+        }
+    ]
+}
+
 var specListModule = angular.module('specListModule', ['ngResource']);
 
-specListModule.controller('specListController', ['$scope', '$resource',
-    function($scope, $resource) {
-        $scope.pressgangRestAPI = $resource(SERVER + REST_BASE + "/contentspecs/get/json/all");
+specListModule.factory('getAllSpecs', ['$resource', function($resource) {
+        return $resource(SERVER + REST_BASE + "/contentspecs/get/json/all?expand=" + encodeURIComponent(JSON.stringify(SPEC_COLLECTION_EXPAND)));
+    }
+]);
+
+specListModule.controller('specListController', ['$scope', 'getAllSpecs',
+    function($scope, getAllSpecs) {
+        getAllSpecs.query(function(data) {
+                $scope.allSpecs = data;
+            }
+        );
     }
 ]);
