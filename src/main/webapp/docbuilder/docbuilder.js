79314f4fe49074d284c9998876f8f33aa1073532
Matthew Casperson <mcaspers@redhat.com>
2014-06-03 06:54:09 +1000

    Fixing spec list page

--- src/main/webapp/docbuilder/docbuilder.js
+++ src/main/webapp/docbuilder/docbuilder.js
@@ -1096,4 +1096,31 @@ docbuilderModule.controller('docbuilderController', [
         }
     }
 ]);
+/*var qs = (function(a) {
+if (a == "") return {};
+var b = {};
+for (var i = 0; i < a.length; ++i)
+{
+var p=a[i].split('=');
+if (p.length != 2) continue;
+b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+}
+return b;
+})(window.location.search.substr(1).split('&'));
+jQuery(document).ready(function(){
+try {
+if (qs["specId"] !== undefined) {
+var specId = parseInt(qs["specId"]);
+if (!isNaN(specId)) {
+var docBuilderLive = new DocBuilderLive(specId);
+} else {
+throw "The book could not be displayed because the specId query parameter is not an integer.";
+}
+} else {
+throw "The book could not be displayed because the specId query parameter is missing.";
+}
+} catch (ex) {
+updateInitialMessage(ex, false);
+}
+})  */
 //# sourceMappingURL=docbuilder.js.map
